a5efebf06d12b06e22556b503b5eb929986bd28a
yscs18 <yanshuo18@163.com>
2016-11-08 21:13:43 +0800

    first DateBase

---
+++ DataBase/src/bookAdmin/Login.java
@@ -0,0 +1,18 @@
+package bookAdmin;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+class Login extends  JFrame implements ActionListener{
+	Container cp=null;
+	JFrame f=null;
+	JButton j1,j2,j3;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
